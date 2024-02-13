44cb6361964ffb47a0310eca2c18af7d81eba81c
Arpan Das <dasarpan471@gmail.com>
2024-02-13 20:32:43 +0530

    login completed

--- server/controllers/authController.go
+++ server/controllers/authController.go
@@ -22,6 +22,17 @@ func HashPassword(password string) string {
 	return string(bytes)
 }
 
+func verifyPassword(providedPassword string, userPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	msg := ""
+	if err != nil {
+		check = false
+		msg = "password is incorrect"
+	}
+	return check, msg
+}
+
 func Signup() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(context.TODO(), 100*time.Second)
@@ -69,5 +80,40 @@ func Signup() gin.HandlerFunc {
 		defer cancel()
 
 		c.JSON(http.StatusCreated, gin.H{"user": userObj, "token": token})
+	}
+}
+
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.TODO(), 100*time.Second)
+		var user, foundUser model.User
+
+		if err := c.BindJSON(&user); err != nil {
+			helpers.ErrorResponse(c, http.StatusBadRequest, err.Error())
+		}
+
+		err := userCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser)
+		defer cancel()
+
+		if err != nil {
+			helpers.ErrorResponse(c, http.StatusInternalServerError, "Email is Incorrect")
+			return
+		}
+
+		passwordIsValid, msg := verifyPassword(*foundUser.Password, *user.Password)
+		defer cancel()
+		if !passwordIsValid {
+			helpers.ErrorResponse(c, http.StatusInternalServerError, msg)
+			return
+		}
+		if foundUser.Email == nil {
+			helpers.ErrorResponse(c, http.StatusInternalServerError, "user not found")
+			return
+		}
+		token, err := helpers.GenerateToken(*foundUser.Email, *foundUser.FirstName, *foundUser.LastName, foundUser.ID.Hex())
+		if err != nil {
+			helpers.ErrorResponse(c, http.StatusInternalServerError, err.Error())
+		}
+		c.JSON(http.StatusOK, gin.H{"user": foundUser, "token": token})
 	}
 }
