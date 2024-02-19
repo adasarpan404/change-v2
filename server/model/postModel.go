9cf0dd40a7cec3a1b1f6072748a2dffd169710e8
Arpan Das <dasarpan471@gmail.com>
2024-02-19 06:52:37 +0530

    post model created

--- server/model/postModel.go
+++ server/model/postModel.go
@@ -6,11 +6,9 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type UserRelationShipModel struct {
+type Post struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	Follower  primitive.ObjectID `bson:"follower"`
-	Following primitive.ObjectID `bson:"following"`
-	User      *User              `bson:"user,omitempty"`
+	Content   *string            `bson:"content"`
 	CreatedAt time.Time          `bson:"createdAt"`
 	UpdatedAt time.Time          `bson:"updatedAt"`
 }
