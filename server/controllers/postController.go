9dfd72d9141fbc6b77fede0f9e83977e76e6a26a
Arpan Das <dasarpan471@gmail.com>
2024-03-01 20:06:42 +0530

    ✅post controller completed

--- server/controllers/postController.go
+++ server/controllers/postController.go
@@ -1,1 +1,27 @@
 package controllers
+
+import "github.com/gin-gonic/gin"
+
+func CreatePost() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetPost() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdatePost() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func DeletePost() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
