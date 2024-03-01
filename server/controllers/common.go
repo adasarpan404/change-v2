eaf3d3aafe3b1506292441b1c8461d5d531d3cb8
Arpan Das <dasarpan471@gmail.com>
2024-03-01 20:05:10 +0530

    ✅ post interaction controller

--- server/controllers/common.go
+++ server/controllers/common.go
@@ -11,3 +11,9 @@ var validate = validator.New()
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 
 var relationShipCollection *mongo.Collection = database.OpenCollection(database.Client, "relationShip")
+
+var postCollection *mongo.Collection = database.OpenCollection(database.Client, "post")
+
+var likeCollection *mongo.Collection = database.OpenCollection(database.Client, "like")
+
+var commentCollection *mongo.Collection = database.OpenCollection(database.Client, "comment")
---
+++ server/controllers/postInteractionController.go
@@ -0,0 +1,51 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func CeateLike() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetLikes() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdateLike() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func DeleteLike() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateComment() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetComments() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdateComment() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func DeleteComment() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
